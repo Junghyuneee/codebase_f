a9dbe672ef781663f7e578b2e9911423636a81e0
pinusdensiflora <101383185+pinusdensiflora@users.noreply.github.com>
2024-11-22 17:35:31 +0900

    test

--- src/components/store/Test.jsx
+++ src/components/store/Test.jsx
@@ -1,103 +1,205 @@
-/*banner mini
-2024 11 15
-배다원*/
-import React from 'react';
+/* store
+배다원 
+2024 10 30
+*/
+
+
+
+import React, { useEffect, useState, useRef, Outlet } from 'react';
 import { Link } from "react-router-dom";
+// nodejs library that concatenates classes
+
+//nav 테스트
+import Banner from "./Banner_mini";
+import Banner_mini from "./Banner_mini";
+import img from "../../assets/img/theme/img-1-1200x1000.jpg";
+
+import apiClient from '@/api/apiClient';
+
+
 import {
-    UncontrolledCollapse,
-    DropdownMenu,
-    DropdownItem,
-    DropdownToggle,
-    UncontrolledDropdown,
-    NavbarBrand,
-    Navbar,
-    NavItem,
-    NavLink,
-    Nav,
+    Button,
+    Card,
+    CardImg,
+    ButtonGroup,
+    CardBody,
+    FormGroup,
+
+    Input,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroup,
     Container,
     Row,
     Col,
+    Badge,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    UncontrolledDropdown
 } from "reactstrap";
 
-import img from "../../assets/img/brand/argon-react.png";
 
-function Banner_mini() {
+function OneProjectCard({ name, price }) {
+
     return (
         <>
+            <Card className="bg-white shadow border-0 card-lift--hover">
 
-            <Navbar className="navbar-dark bg-info" expand="lg">
-                <Container>
-                    <NavbarBrand href="#pablo" onClick={(e) => e.preventDefault()}>
-                        Primary Color
-                    </NavbarBrand>
-                    <button className="navbar-toggler" id="navbar-primary">
-                        <span className="navbar-toggler-icon" />
-                    </button>
-                    <UncontrolledCollapse navbar toggler="#navbar-primary">
-                        <div className="navbar-collapse-header">
-                            <Row>
-                                <Col className="collapse-brand" xs="6">
-                                    <Link to="/">
-                                        <img
-                                            alt="..."
-                                            src={img}
-                                        />
-                                    </Link>
-                                </Col>
-                                <Col className="collapse-close" xs="6">
-                                    <button className="navbar-toggler" id="navbar-primary">
-                                        <span />
-                                        <span />
-                                    </button>
-                                </Col>
-                            </Row>
-                        </div>
-                        <Nav className="ml-lg-auto" navbar>
-                            <NavItem>
-                                <NavLink href="/store">
-                                    상점 홈 <span className="sr-only">(current)</span>
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/store/cart">
-                                    장바구니
-                                </NavLink>
-                            </NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav>마이페이지 <i className="ni ni-tag" /></DropdownToggle>
-                                <DropdownMenu
-                                    aria-labelledby="navbar-primary_dropdown_1"
-                                    right
-                                >
-                                    <DropdownItem
-                                        href="/store/test"
-                                        
-                                    >
-                                        테스트페이지
-                                    </DropdownItem>
-                                    <DropdownItem
-                                        href="/store"
-                                        
-                                    >
-                                        메인화면
-                                    </DropdownItem>
-                                    <DropdownItem divider />
-                                    <DropdownItem
-                                        href="/login"
-                                        
-                                    >
-                                        로그아웃
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
-                        </Nav>
-                    </UncontrolledCollapse>
-                </Container>
-            </Navbar>
+                <blockquote className="card-blockquote p-4">
+                    <CardImg style={{ borderRadius: '10px' }}
+                        alt="..."
+                        src={img}
+                        top
+                    />
+                    <h4 className="display-4 font-weight-bold text-black"
+                        style={{
+                            display: '-webkit-box',          // Flexbox 사용
+                            WebkitBoxOrient: 'vertical',     // 세로 방향으로 정렬
+                            WebkitLineClamp: 2,              // 두 줄까지만 표시
+                            overflow: 'hidden',               // 넘치는 텍스트 숨기기
+                            textOverflow: 'ellipsis',         // 넘치는 텍스트를 ...으로 표시
+                            width: '100%',                    // 부모 폭에 맞게 설정
+                            margin: 0                         // 기본 마진 제거
+                        }}
+                    >
+                        {name}
+                    </h4>
+                    <br /><br />
+
+
+                </blockquote>
+
+                <Badge color="secondary" pill className="mr-1"
+                    style={{
+                        fontSize: '14px',
+                        position: 'absolute', // 절대 위치 설정
+                        bottom: '30px',      // 하단에서 10px
+                        left: '30px'        // w좌측에서 10px
+                    }}>
+                    {price}원
+                </Badge>
+            </Card>
 
 
         </>
     );
+
 }
 
-export default Banner_mini;
+function GetProject() {
+    //토큰 사용 테스트
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        // 데이터 가져오기
+        apiClient.get('http://localhost:8080/api/store')
+            .then((response) => {
+                setProjects(response.data); // 응답 데이터 설정
+            })
+            .catch((error) => {
+                console.error('API 호출 에러:', error);
+            });
+    }, []);
+
+
+    return projects;
+}
+
+
+function ProjectCards() {
+    const initprojects = GetProject();
+ 
+    const [sortOption, setSortOption] = useState('최신순');//기본값 (최신순)
+
+    const sortedProjects = [...initprojects].sort((a, b) => {
+        console.log("sortOption : ", sortOption);
+        switch (sortOption) {
+            case '최신순':
+                return b.id - a.id; // ID로 최신순 정렬
+            case '조회순':
+                return b.hit - a.hit; // 조회수로 정렬
+            case '이름순':
+                  return a.name.localeCompare(b.name, 'ko'); // 가나다 정렬
+            default:
+                return 0;
+        }
+
+    });
+
+    const projects = sortedProjects;
+    return (
+        <>
+
+            
+            <section className="section bg-secondary pt-4"
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center', // 가로 가운데 정렬
+                    padding: '0px',
+                    maxWidth: '100vw'
+                }}>
+                <div style={{ marginLeft: '10%', marginRight: '10%' }}>
+                
+      <ButtonGroup>
+        <Button
+          color="primary"
+          outline
+          onClick={() => setSortOption('최신순')}
+          active={sortOption === '최신순'}
+        >
+          최신순
+        </Button>
+        <Button
+          color="primary"
+          outline
+          onClick={() => setSortOption('조회순')}
+          active={sortOption === '조회순'}
+        >
+          조회순
+        </Button>
+        <Button
+          color="primary"
+          outline
+          onClick={() => setSortOption('이름순')}
+          active={sortOption === '이름순'}
+        >
+          이름순
+        </Button>
+
+      </ButtonGroup>
+
+                    <Row className="row-grid align-items-center">
+
+                        {projects.map((project) => (
+                            <Col xs="12" sm="12" md="6" lg="4" xl="3" className='p-2'>
+                                {console.log(project)}
+                                <a href={`/store/${project.id}`} ><OneProjectCard name={project.name} price={project.price} /></a>
+                            </Col>
+                        ))}
+
+                    </Row>
+                </div>
+            </section>
+        </>
+    );
+}
+
+function Page() {
+
+    return (
+        <>
+            <Banner />
+            <Banner_mini />
+            <main>
+
+                <ProjectCards />
+            </main>
+        </>
+    );
+
+}
+
+export default Page;
+
+
