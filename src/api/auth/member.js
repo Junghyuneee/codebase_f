87c5e2205ef0f4f28c82a0263e5bcab4d1e3a417
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-12-10 10:06:37 +0900

    Fix: member api separation

--- src/api/auth/member.js
+++ src/api/auth/member.js
@@ -1,13 +1,13 @@
 import apiClient from "@/api/apiClient.js";
 
 export const searchMember = async (param) => {
-    const response = await apiClient.get(`/auth/search/${param}`);
+    const response = await apiClient.get(`/member/search/${param}`);
     console.log(response.data);
     return response.data;
 }
 
 export const getMember = async (param) => {
-    const response = await apiClient.get("/auth/profile" + param?"/"+param:"");
+    const response = await apiClient.get("/member/profile" + param?"/"+param:"");
     console.log(response.data);
     return response.data;
 }
