3b3a07cac3223fec26e2bcea14574280a6a17e62
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-12-11 16:02:44 +0900

    Feat: custom hook of subscription chatroom

---
+++ src/hooks/chat/useSubscribeToChatRoom.js
@@ -0,0 +1,49 @@
+import {useEffect, useRef, useState} from "react";
+import {getMessages} from "@/api/chat/message.js";
+import {getAccessToken} from "@/api/auth/getset.js";
+import {exitChatroom} from "@/api/chat/chatroom.js";
+
+const useSubscribeToChatRoom = (chatRoomId, stompClient) => {
+
+    const [messages, setMessages] = useState([]);
+    const subscriptionRef = useRef(null);
+
+    useEffect(() => {
+        console.log(chatRoomId)
+        const get = async () => {
+            const response = await getMessages(chatRoomId);
+            setMessages(response);
+        }
+        
+        if(chatRoomId && stompClient.connected) {
+            get();
+            if (subscriptionRef.current) {
+                console.log('Unsubscribing from previous chat room');
+                subscriptionRef.current.unsubscribe();
+            }
+
+            console.log('Connected to chat room', chatRoomId);
+            subscriptionRef.current = stompClient.subscribe(
+                `/sub/chats/${chatRoomId}`,
+                (chatMessage) => {
+                    setMessages((prevMessages) => [...prevMessages, JSON.parse(chatMessage.body)]);
+                },
+                {Authorization: `${getAccessToken()}`}
+            );
+        }
+
+        return () => {
+            if(subscriptionRef.current) {
+                console.log('Cleaning up subscription for chat room:', chatRoomId);
+                exitChatroom(chatRoomId);
+                subscriptionRef.current.unsubscribe();
+                setMessages([]);
+            }
+        }
+        
+    }, [chatRoomId, stompClient]);
+
+    return messages;
+}
+
+export default useSubscribeToChatRoom;
