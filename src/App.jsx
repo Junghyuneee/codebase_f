2853313dc29a513494f1f878381392530208c782
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-12-18 16:54:50 +0900

    Fix: wait till zustand store load new authdata

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./assets/vendor/font-awesome/css/font-awesome.min.css";
 import "./assets/css/argon-design-system-react.css";
 import './App.css';
 
-import { Route, Routes } from "react-router-dom"
+import {Route, Routes} from "react-router-dom"
 import MainPage from "./pages/main/MainPage"
 import LoginModal from './components/auth/LoginModal';
 import RegisterModal from "./components/auth/RegisterModal";
@@ -23,32 +23,48 @@ import ChatPage from "@/pages/chat/ChatPage.jsx";
 import Profile from "@/pages/member/Profile.jsx";
 import ChangeInfo from "@/pages/member/ChangeInfo.jsx";
 import ChangePWD from "./pages/member/ChangePWD";
-
+import useAuthStore from "@/zustand/authStore.js";
+import {useEffect, useState} from "react";
 
 function App() {
+    const authStore = useAuthStore();
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        const checkRefreshToken = async () => {
+            setIsLoading(true)
+            await authStore.initializeAuth();
+            setIsLoading(false)
+        }
+        checkRefreshToken();
+    }, []);
+
+    if(isLoading){
+        return <div>Loading...</div>;
+    }
 
     return (
 
         <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/login" element={<LoginModal />} />
-            <Route path="/register" element={<RegisterModal />} />
+            <Route path="/" element={<MainPage/>}/>
+            <Route path="/login" element={<LoginModal/>}/>
+            <Route path="/register" element={<RegisterModal/>}/>
 
-            <Route path="/store/*" exact element={<Store />} />
+            <Route path="/store/*" exact element={<Store/>}/>
 
-            <Route path="/admin/*" exact element={<Admin />} />
+            <Route path="/admin/*" exact element={<Admin/>}/>
 
-            <Route path="/team" exact element={<Team />} />
-            <Route path="/teamdetail/:id" exact element={<Teamdetail />} />
-            <Route path="/review/*" exact element={<Review />} />
-            <Route path="/oauth" element={<OauthCallbackPage />} />
-            <Route path="/post/*" element={<Post />} />
+            <Route path="/team" exact element={<Team/>}/>
+            <Route path="/teamdetail/:id" exact element={<Teamdetail/>}/>
+            <Route path="/review/*" exact element={<Review/>}/>
+            <Route path="/oauth" element={<OauthCallbackPage/>}/>
+            <Route path="/post/*" element={<Post/>}/>
 
-            <Route path="/profile/:id?" element={<Profile />} />
-            <Route path={"/changeinfo"} element={<ChangeInfo />} />
-            <Route path={"/changepwd"} element={<ChangePWD />} />
+            <Route path="/profile/:id?" element={<Profile/>}/>
+            <Route path={"/changeinfo"} element={<ChangeInfo/>}/>
+            <Route path={"/changepwd"} element={<ChangePWD/>}/>
 
-            <Route path="/chat" element={<ChatPage />} />
+            <Route path="/chat" element={<ChatPage/>}/>
         </Routes>
 
     )
