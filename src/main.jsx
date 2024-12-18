0aadeda07000ad764e437dec1ca665f0aee1e1e8
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-12-18 13:11:22 +0900

    Fix: Disable Strict Mode

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./assets/css/argon-design-system-react.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter future={{ v7_startTransition: true }}>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
