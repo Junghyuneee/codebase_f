03a0c95ec28629f920467d04d1e57ccb6d7e53f9
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-12-30 10:38:27 +0900

    Fix: 401 error handling logic

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -19,14 +19,14 @@ apiClient.interceptors.request.use(config => {
     }
 )
 
+// 토큰이 있는 상태에서 401에러 발생하면 토큰 다시 받아야댐
 apiClient.interceptors.response.use((response) => {
     return response;
 }, (error) => {
     if (error.response && error.response.status === 401) {
         console.error('Error initializing auth:', error);
-        if (error.response?.status === 401) {
-            window.location.reload();
-        }
+        authStore.getState().clearAuthData();
+        window.location.replace("/login");
     }
 }
 )
